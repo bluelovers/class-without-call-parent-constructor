f67de4494206110013d3e6dedde2443a7e5b2736
bluelovers <codelovers@users.sourceforge.net>
2021-07-01 18:19:57 +0800

    chore(release): publish

--- dist/class-without-call-parent-constructor.cjs.development.js
+++ dist/class-without-call-parent-constructor.cjs.development.js
@@ -2,110 +2,16 @@
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
-function _classCallCheck(instance, Constructor) {
-  if (!(instance instanceof Constructor)) {
-    throw new TypeError("Cannot call a class as a function");
-  }
-}
-
-function _inherits(subClass, superClass) {
-  if (typeof superClass !== "function" && superClass !== null) {
-    throw new TypeError("Super expression must either be null or a function");
-  }
-
-  subClass.prototype = Object.create(superClass && superClass.prototype, {
-    constructor: {
-      value: subClass,
-      writable: true,
-      configurable: true
-    }
-  });
-  if (superClass) _setPrototypeOf(subClass, superClass);
-}
-
-function _getPrototypeOf(o) {
-  _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) {
-    return o.__proto__ || Object.getPrototypeOf(o);
-  };
-  return _getPrototypeOf(o);
-}
-
-function _setPrototypeOf(o, p) {
-  _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) {
-    o.__proto__ = p;
-    return o;
-  };
-
-  return _setPrototypeOf(o, p);
-}
-
-function _isNativeReflectConstruct() {
-  if (typeof Reflect === "undefined" || !Reflect.construct) return false;
-  if (Reflect.construct.sham) return false;
-  if (typeof Proxy === "function") return true;
-
-  try {
-    Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function () {}));
-    return true;
-  } catch (e) {
-    return false;
-  }
-}
-
-function _assertThisInitialized(self) {
-  if (self === void 0) {
-    throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
-  }
-
-  return self;
-}
-
-function _possibleConstructorReturn(self, call) {
-  if (call && (typeof call === "object" || typeof call === "function")) {
-    return call;
-  }
-
-  return _assertThisInitialized(self);
-}
-
-function _createSuper(Derived) {
-  var hasNativeReflectConstruct = _isNativeReflectConstruct();
-
-  return function _createSuperInternal() {
-    var Super = _getPrototypeOf(Derived),
-        result;
-
-    if (hasNativeReflectConstruct) {
-      var NewTarget = _getPrototypeOf(this).constructor;
-
-      result = Reflect.construct(Super, arguments, NewTarget);
-    } else {
-      result = Super.apply(this, arguments);
-    }
-
-    return _possibleConstructorReturn(this, result);
-  };
-}
-
 function createNewTargetObject(createNew) {
   return Object.create(createNew.prototype);
 }
 function classWithoutCallParentConstructor(parentClass) {
-  return /*#__PURE__*/function (_parentClass) {
-    _inherits(_class, _parentClass);
-
-    _createSuper(_class);
-
-    function _class() {
-      var _this;
-
-      _classCallCheck(this, _class);
-
-      return _possibleConstructorReturn(_this, createNewTargetObject(this instanceof _class ? this.constructor : void 0));
+  return class extends parentClass {
+    constructor() {
+      return createNewTargetObject(new.target);
     }
 
-    return _class;
-  }(parentClass);
+  };
 }
 
 exports.classWithoutCallParentConstructor = classWithoutCallParentConstructor;
